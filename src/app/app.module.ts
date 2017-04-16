dff178303e8d4c63cf5d57436caf1a69f4beb839
demoemailid1101 <demo.emailid1101@gmail.com>
2017-04-16 12:11:11 +0530

    Initial commit.

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { BrowserModule } from '@angular/platform-browser';
+import { cricscore } from './app.component';
+import { Page1 } from '../pages/page1/page1';
+import { Page2 } from '../pages/page2/page2';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { HttpModule } from '@angular/http';
+
+@NgModule({
+  declarations: [
+    cricscore,
+    Page1,
+    Page2
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    IonicModule.forRoot(cricscore)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    cricscore,
+    Page1,
+    Page2
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
