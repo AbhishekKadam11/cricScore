895565ca50139688dcfb875ff777b259e641632c
demoemailid1101 <demo.emailid1101@gmail.com>
2017-04-20 21:46:24 +0530

    changes in server js file.

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ var app = express();
 app.use(express.static(path.resolve(__dirname, 'www')));
 
 // Start server
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-  , ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-app.listen(port, ip, function () {
-  console.log('Express server listening on %d', port);
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), function() {
+  console.log('listning to Port', app.get('port'));
 });
